7cc9a3151b433eb85aa542d24d6fc4ccb3682274
James418-alt <ebifegha123@gmail.com>
2024-08-06 15:58:05 +0100

    update

--- utils/dbConfig.ts
+++ utils/dbConfig.ts
@@ -2,7 +2,7 @@ import { clear } from "console";
 import { connect } from "mongoose";
 
 export const dbConfig = async () => {
-  const url = process.env.DB_URL as string;
+  const url = process.env.LIVE_URL as string;
   await connect(url).then(() => {
     clear();
     console.log("Serevr Up!");
